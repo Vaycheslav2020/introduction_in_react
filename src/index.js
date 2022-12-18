388f65aee9e4c97309b4079286dba386d954e0c7
Vaycheslav <slavonkozak@mail.ru>
2022-12-18 17:33:29 +0300

    add project in git

--- src/index.js
+++ src/index.js
@@ -1,17 +1,214 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        key={i}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const listBoard = [];
+    let count = 0;
+    for (let i = 0; i < 3; i++) {
+      const listSquare = [];
+      for (let t = 0; t < 3; t++) {
+        listSquare.push(this.renderSquare(count));
+        count += 1;
+      }
+      listBoard.push(
+        <div key={i} className="board-row">
+          {listSquare}
+        </div>
+      );
+    }
+    return <div>{listBoard}</div>;
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+      indexBtn: 0,
+      col: 0,
+      row: 0,
+    };
+  }
+
+  getCol(index) {
+    let posCol;
+    const col = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    for (let i = 0; i < col.length; i++) {
+      const el = col[i];
+      for (let j = 0; j < el.length; j++) {
+        const [a, b, c] = el;
+        if (index === a || index === b || index === c) {
+          posCol = i + 1;
+        }
+      }
+    }
+    this.setState({
+      col: posCol,
+    })
+  }
+
+  getRow(index) {
+    let posRow;
+    const row = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
+    
+    for (let i = 0; i < row.length; i++) {
+      const el = row[i];
+      for (let j = 0; j < el.length; j++) {
+        const [a, b, c] = el;
+        if (index === a || index === b || index === c) {
+          posRow = i + 1;
+        }
+      }
+    }
+    this.setState({
+      row: posRow,
+    })
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      const line = calculateWinner(squares)
+      if (line.length !== null) {
+        for (let i = 0; i < line.length; i++) {
+          const el = line[i];
+          let list = document.querySelectorAll(".square")
+          list[el].style.background = "red"
+        } 
+      } else {
+        document.write("игра закончилась в ничью")
+      }
+      return;
+    }
+    this.getCol(i)
+    this.getRow(i)
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+      indexBtn: i,
+    });
+  }
+
+  jumpTo(step) {
+    document.querySelectorAll(".square").forEach(sqr => {
+      sqr.style.background = "none"
+    })
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ? "Перейти к ходу" + " " + move : "К началу игры";
+      const pos = "колонка" + this.state.col + ", ряд:" + this.state.row
+      return (
+        <li key={move}>
+          <button
+            className={`sidebar ${
+              this.state.stepNumber === move ? "active" : ""
+            }`}
+            onClick={() => this.jumpTo(move)}
+          >
+            {desc}
+          </button>
+          <span>{pos}</span>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Выиграл" + winner;
+    } else {
+      status = "Следующий ход:" + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return lines[i];
+    }
+  }
+  return null;
+}
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
